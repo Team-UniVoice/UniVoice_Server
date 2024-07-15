02b857cfad4f247cf177bfb0cf9eb6dd5c5aa475
eeddiinn <gdw4013@naver.com>
2024-07-16 03:19:51 +0900

    [FIX] createdAt반환시 밀리초 빼도록 수정 #55

--- src/main/java/sopt/univoice/infra/persistence/BaseTimeEntity.java
+++ src/main/java/sopt/univoice/infra/persistence/BaseTimeEntity.java
@@ -2,12 +2,14 @@ package sopt.univoice.infra.persistence;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @MappedSuperclass
@@ -16,4 +18,13 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = truncateToSeconds(this.createdAt);
+    }
+
+    private LocalDateTime truncateToSeconds(LocalDateTime dateTime) {
+        return dateTime != null ? dateTime.truncatedTo(ChronoUnit.SECONDS) : null;
+    }
 }
